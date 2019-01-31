6ed428ef9168eb2bc921fc43a6e9b06f18396687
KimHyunJune <35071931+oryondark@users.noreply.github.com>
2019-01-31 13:13:06 +0900

    Recursive_directory_parser using Generator

--- Python/python_directory_parser.py
+++ Python/python_directory_parser.py
@@ -1,10 +1,18 @@
-# recursive function
+'''
+Recursive Generator Directory Parser
+'''
 def directory_parser(target):
     list = os.listdir(target)
     for item in list:
         path = os.path.join(target, item)
         if os.path.isdir(path) == True:
             print("/{} : it's directory".format(item))
-            directory_parser(path)
+            yield directory_parser(path)
         else:
             print("{} : is full_path".format(path))
+            yield path
+
+path = directory_parser("precede_path")
+for inner_path in path:
+    for item in inner_path:
+        print(item)
